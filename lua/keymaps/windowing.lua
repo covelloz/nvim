96e58a30b491d7549bd2bba8ad3a3130920b915d
Michael Covello <michael.covello@campspot.com>
2024-06-26 17:35:14 -0500

    Add a helpful split-view keymap

--- lua/keymaps/windowing.lua
+++ lua/keymaps/windowing.lua
@@ -1,7 +1,10 @@
 local keymap = vim.keymap.set
 
-keymap('n', '<C-h>', '<C-w>h', { desc = 'switch window left' })
-keymap('n', '<C-l>', '<C-w>l', { desc = 'switch window right' })
-keymap('n', '<C-j>', '<C-w>j', { desc = 'switch window down' })
-keymap('n', '<C-k>', '<C-w>k', { desc = 'switch window up' })
+keymap('n', '<C-h>', '<C-w>h', { noremap = true, silent = true, desc = 'switch window left' })
+keymap('n', '<C-l>', '<C-w>l', { noremap = true, silent = true, desc = 'switch window right' })
+keymap('n', '<C-j>', '<C-w>j', { noremap = true, silent = true, desc = 'switch window down' })
+keymap('n', '<C-k>', '<C-w>k', { noremap = true, silent = true, desc = 'switch window up' })
+
+keymap('n', '<leader>sv', '<C-w>H', { noremap = true, silent = true, desc = 'switch to vertical split' })
+keymap('n', '<leader>sh', '<C-w>K', { noremap = true, silent = true, desc = 'switch to horizontal split' })
 
