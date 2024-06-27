1926d33777f60693739e89a11108a032667a2e71
Michael Covello <michael.covello@campspot.com>
2024-06-27 15:25:06 -0500

    Remap :nohlsearch to quickly clear search highlights by tapping ESC twice

--- lua/keymaps/general.lua
+++ lua/keymaps/general.lua
@@ -2,4 +2,5 @@ local keymap = vim.keymap.set
 
 keymap('i', 'jj', '<ESC>', { noremap = true, silent = true, desc = 'insert-mode to normal-mode' })
 keymap({'n', 'v'}, 'y', '"+y', { noremap = true, silent = true, desc = 'yank copies to system clipboard'})
+keymap('n', '<ESC><ESC>', ':nohlsearch<CR>', { noremap = true, silent = true, desc = 'clear search highlights'} )
 
