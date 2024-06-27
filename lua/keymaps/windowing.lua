0d2dcd6e9feab8ea09bc3d6551998000ce530ebd
Michael Covello <michael.covello@campspot.com>
2024-06-27 13:44:32 -0500

    Add some helpful window resizing hotkeys

--- lua/keymaps/windowing.lua
+++ lua/keymaps/windowing.lua
@@ -1,3 +1,4 @@
+local vim = vim
 local keymap = vim.keymap.set
 
 keymap('n', '<C-h>', '<C-w>h', { noremap = true, silent = true, desc = 'switch window left' })
@@ -10,4 +11,41 @@ keymap('n', '<leader>sh', '<C-w>K', { noremap = true, silent = true, desc = 'swi
 
 keymap('n', '<leader>c', ':ToggleTerm <CR>', { noremap = true, silent = true, desc = 'open terminal'})
 keymap('t', '<ESC>', '<ESC><C-\\><C-n>', { noremap = true, silent = true, desc = 'escape terminal mode'})
+
+function ResizeWindowVerticalIncrease()
+  local count = vim.v.count
+  if count == 0 then
+    count = 20
+  end
+  vim.cmd("vertical resize +" .. count)
+end
+
+function ResizeWindowVerticalDecrease()
+  local count = vim.v.count
+  if count == 0 then
+    count = 20
+  end
+  vim.cmd("vertical resize -" .. count)
+end
+
+function ResizeWindowHorizontalIncrease()
+  local count = vim.v.count
+  if count == 0 then
+    count = 20
+  end
+  vim.cmd("resize +" .. count)
+end
+
+function ResizeWindowHorizontalDecrease()
+  local count = vim.v.count
+  if count == 0 then
+    count = 20
+  end
+  vim.cmd("resize -" .. count)
+end
+
+keymap('n', '<leader>rr', ':<C-u>lua ResizeWindowVerticalIncrease()<CR>', { noremap = true, silent = true })
+keymap('n', '<leader>rl', ':<C-u>lua ResizeWindowVerticalDecrease()<CR>', { noremap = true, silent = true })
+keymap('n', '<leader>ru', ':<C-u>lua ResizeWindowHorizontalIncrease()<CR>', { noremap = true, silent = true })
+keymap('n', '<leader>rd', ':<C-u>lua ResizeWindowHorizontalDecrease()<CR>', { noremap = true, silent = true })
 
