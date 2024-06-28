e0a9efd6218c245f8dc7705238941fbd3fec81f9
Michael Covello <michael.covello@campspot.com>
2024-06-28 15:24:10 -0500

    Add TermSelect hotkey

--- lua/plugins/setup/toggleterm.lua
+++ lua/plugins/setup/toggleterm.lua
@@ -1,3 +1,8 @@
+local keymap = vim.keymap.set
+
+keymap('n', '<C-u><leader>c', ':ToggleTerm<CR>', { noremap = true, silent = true, desc = 'open terminal'})
+keymap('n', '<leader>sc', ':TermSelect<CR>', { noremap = true, silent = true, desc = 'select terminal to open' })
+
 require("toggleterm").setup {
   size = 15,
   hide_numbers = true,
