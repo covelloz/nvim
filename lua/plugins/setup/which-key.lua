db2630a3bcb0bff4b9203642b8fb0480b9d704db
Michael Covello <michael.covello@campspot.com>
2024-06-27 22:12:17 -0500

    Small fix to WhichKey hotkeys

--- lua/plugins/setup/which-key.lua
+++ lua/plugins/setup/which-key.lua
@@ -6,7 +6,7 @@ opt.timeoutlen = 300
 
 local wk = require('which-key')
 
-keymap('n', '<leader>wk', ':WhichKey', { desc = 'Open WhichKey' })
+keymap('n', '<leader>wk', ':WhichKey<CR>', { desc = 'Open WhichKey' })
 
 -- document custom mappings
 wk.setup({})
