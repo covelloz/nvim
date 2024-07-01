9fe49f6f8e770d76cd46d7768fc13ad816759d1e
Michael Covello <covelloz.michael@gmail.com>
2024-06-30 20:48:51 -0500

    Update bufferline hotkeys for closing buffers (using q instead of c - easier)

--- lua/plugins/setup/bufferline.lua
+++ lua/plugins/setup/bufferline.lua
@@ -6,10 +6,10 @@ keymap('n', '<leader>bh', ':BufferLineMovePrev<CR>', { noremap = true, silent = 
 keymap('n', '<leader>bl', ':BufferLineMoveNext<CR>', { noremap = true, silent = true, desc = 'move current buffer right' })
 keymap('n', '<leader>bp', ':BufferLineTogglePin<CR>', { noremap = true, silent = true, desc = 'toggle buffer pin' })
 
-keymap('n', '<leader>bcc', ':bd<CR>:bp<CR>', { noremap = true, silent = true, desc = 'close current buffer & go to previous' })
-keymap('n', '<leader>bcr', ':BufferLineCloseRight<CR>', { noremap = true, silent = true, desc = 'close all buffers to the right' })
-keymap('n', '<leader>bcl', ':BufferLineCloseLeft<CR>', { noremap = true, silent = true, desc = 'close all buffers to the left' })
-keymap('n', '<leader>bco', ':BufferLineCloseOthers<CR>', { noremap = true, silent = true, desc = 'close all except current buffer' })
+keymap('n', '<leader>bqq', ':bd<CR>:bp<CR>', { noremap = true, silent = true, desc = 'close current buffer & go to previous' })
+keymap('n', '<leader>bqr', ':BufferLineCloseRight<CR>', { noremap = true, silent = true, desc = 'close all buffers to the right' })
+keymap('n', '<leader>bql', ':BufferLineCloseLeft<CR>', { noremap = true, silent = true, desc = 'close all buffers to the left' })
+keymap('n', '<leader>bqo', ':BufferLineCloseOthers<CR>', { noremap = true, silent = true, desc = 'close all except current buffer' })
 keymap('n', '<leader>bQ', ':bufdo bdelete<CR>', { noremap = true, silent = true, desc = 'close all buffers' })
 
 require("bufferline").setup {
