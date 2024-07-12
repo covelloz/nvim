8489b6c794d205b6fdc664408726dd15896f360d
Michael Covello <michael.covello@campspot.com>
2024-07-12 17:50:31 -0500

    Simplify tsserver-conf a bit

--- lua/plugins/setup/configs/lsp/tsserver-conf.lua
+++ lua/plugins/setup/configs/lsp/tsserver-conf.lua
@@ -3,7 +3,6 @@ local default_config = require('plugins.setup.configs.lsp.default-conf')
 
 return vim.tbl_deep_extend("force", default_config, {
   init_options = {
-    hostInfo = "neovim",
     preferences = {
       importModuleSpecifierPreference = "non-relative",
     },
