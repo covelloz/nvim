41a957c3018caa1a7a0ed7d1717f35cf02265c4b
Michael Covello <michael.covello@campspot.com>
2024-07-25 14:39:05 -0500

    Add telescope keybind to find method/variable references to cover for gaps in LSP_reference

--- lua/plugins/setup/nvim-telescope.lua
+++ lua/plugins/setup/nvim-telescope.lua
@@ -1,11 +1,32 @@
+local vim = vim
 local keymap = vim.keymap.set
-
 local builtin = require('telescope.builtin')
+local ts_utils = require('nvim-treesitter.ts_utils')
 
 keymap('n', '<leader>ff', ':lua require"telescope.builtin".find_files({ hidden = true })<CR>', { desc = 'nvim-telescope find files' })
 keymap('n', '<leader>fg', builtin.live_grep, { desc = 'nvim-telescope grep in files' })
+keymap('n', '<leader>ft', builtin.treesitter, { desc = 'nvim-telescope find treesitter references'})
 keymap('n', '<leader>fb', builtin.buffers, { desc = 'nvim-telescope find buffers' })
 keymap('n', '<leader>fh', builtin.help_tags, { desc = 'nvim-telescope find tags' })
+
+local function get_current_identifier()
+-- Function to get the current identifier under the cursor using Treesitter
+  local node = ts_utils.get_node_at_cursor()
+  if node then
+    local text = ts_utils.get_node_text(node, 0)
+    return text and text[1] or ''
+  end
+  return ''
+end
+
+vim.keymap.set('n', '<leader>fr', function()
+  local identifier = get_current_identifier()
+  if identifier ~= '' then
+    builtin.grep_string({ search = identifier })
+  else
+    print("No identifier found under cursor")
+  end
+end, { desc = 'nvim-telescope find references using Treesitter' })
 
 require('telescope').setup({
 	defaults = {
--- lua/plugins/setup/nvim-tree.lua
+++ lua/plugins/setup/nvim-tree.lua
@@ -20,7 +20,7 @@ require('nvim-tree').setup({
 	},
   actions = {
     open_file = {
-      resize_window = false
+      resize_window = true
     }
   }
 })
