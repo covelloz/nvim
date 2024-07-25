4416062c735db9e6e459ae25d7c8e7a62357225a
Michael Covello <michael.covello@campspot.com>
2024-07-25 15:03:06 -0500

    Add hotkeys for open/close/clear quickfixlist

--- lua/keymaps/general.lua
+++ lua/keymaps/general.lua
@@ -1,3 +1,4 @@
+local vim = vim
 local keymap = vim.keymap.set
 
 keymap('i', 'jj', '<ESC>', { noremap = true, silent = true, desc = 'insert-mode to normal-mode' })
@@ -5,3 +6,7 @@ keymap({ 'n', 'v' }, 'y', '"+y', { noremap = true, silent = true, desc = 'yank c
 keymap('n', '<ESC><ESC>', ':nohlsearch<CR>', { noremap = true, silent = true, desc = 'clear search highlights' })
 keymap({ 'n', 'i' }, '<A-k>', ':m .-2<CR>==', { noremap = true, silent = true, desc = 'move line up' })
 keymap({ 'n', 'i' }, '<A-j>', ':m .1<CR>==', { noremap = true, silent = true, desc = 'move line down' })
+
+keymap('n', '<leader>qo', ':copen<CR>', { noremap = true, silent = true, desc = 'open quickfixlist' })
+keymap('n', '<leader>qq', ':cclose<CR>', { noremap = true, silent = true, desc = 'close quickfixlist' })
+keymap('n', '<leader>qc', function() vim.fn.setqflist({}, 'r') end, { noremap = true, silent = true, desc = 'clear quickfixlist' })
